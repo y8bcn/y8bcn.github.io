77d3efb1e2aa5100d6365183bb177305a50af4bf
coffee <zglovedx@163.com>
2023-09-26 14:03:27 +0800

    Update apk.js

--- apk.js
+++ apk.js
@@ -1,1 +1,68 @@
-g
+var codeToImage = {
+    "111": "https://pica.zhimg.com/80/v2-aa53ff2bb79faf82148c252012431f91_720w.png",
+"112": "https://pic1.zhimg.com/80/v2-4c34eb8ba697c7f69c4838ebcb0350ea_720w.png",
+"113": "https://picx.zhimg.com/80/v2-1ba8d849f6e8de73c581291c9703124a_720w.png",
+"114": "https://picx.zhimg.com/80/v2-88320046a42ab8c3c847557da308e128_720w.png",
+"115": "https://picx.zhimg.com/80/v2-8736ca9a50b1c55b1038d756ac65adc6_720w.png",
+"116": "https://picx.zhimg.com/80/v2-6fc271a98a695eeb8644dae3dcaf4366_720w.png",
+"117": "https://pic1.zhimg.com/80/v2-2bab88c8806e7cef89b74ee9097abcb0_720w.png",
+"118": "https://picx.zhimg.com/80/v2-4b850751441cfe264ce896fccbaf8f92_720w.png",
+"119": "https://pic1.zhimg.com/80/v2-9ef52fa73791494b438793977c657305_720w.png",
+"120": "https://pica.zhimg.com/80/v2-5c9d63506d51ba5c49006ba6345946d7_720w.png",
+"121": "https://pica.zhimg.com/80/v2-9f49b9821b9adc3a0127ea74ca7d171e_720w.png",
+"122": "https://picx.zhimg.com/80/v2-8849816b25b9d0a1b8cf2195357d8569_720w.png",
+"123": "https://picx.zhimg.com/80/v2-0789b6591f1eb48f4c411b9b1e7ac970_720w.png",
+"124": "https://picx.zhimg.com/80/v2-9e5a13b927787b72412f063187fe7528_720w.png",
+"125": "https://pica.zhimg.com/80/v2-92d8955787c7ce69ee7599acacf9559a_720w.png",
+"126": "https://picx.zhimg.com/80/v2-f7dc52c45b865df41610efd601abfa52_720w.png",
+"127": "https://picx.zhimg.com/80/v2-2f3c81c41925686f7c2b8acb003612db_720w.png",
+"128": "https://pic1.zhimg.com/80/v2-ccfc5cfe63b13032e8b610fa1ef7bba0_720w.png",
+"129": "https://picx.zhimg.com/80/v2-3a49dfca813a78fa05562ed2daa74f6a_720w.png",
+"130": "https://pica.zhimg.com/80/v2-6ca9f66f6726ad45771e67379ea63524_720w.png",
+"131": "https://pic1.zhimg.com/80/v2-f15283285520076f4ca1e11948ba1903_720w.png",
+"132": "https://picx.zhimg.com/80/v2-7e114015c86effc838bc22319f48d6ff_720w.png",
+"133": "https://picx.zhimg.com/80/v2-21e190dfe021347d9a6d188114f348a2_720w.png",
+"134": "https://picx.zhimg.com/80/v2-3c471038d0cd39d59c0d95c8cd81b634_720w.png",
+"135": "https://picx.zhimg.com/80/v2-81fa28946f162fac6008e9c124520e3a_720w.png",
+"136": "https://picx.zhimg.com/80/v2-9319a422cd0924a3b4415b735c7770e7_720w.png",
+"137": "https://picx.zhimg.com/80/v2-bb4306ed6a851309470070787027e90b_720w.png",
+"138": "https://pic1.zhimg.com/80/v2-1f73223c88eac20aaabb96aa347cda7e_720w.png",
+"139": "https://picx.zhimg.com/80/v2-69f16d782d36ad71f2602864a97f8c61_720w.png",
+"140": "https://picx.zhimg.com/80/v2-631f730fb93bc3eb9170c766c6b9ba63_720w.png",
+"141": "https://pic1.zhimg.com/80/v2-a44d96b41c656d50e059d34f137e467d_720w.png",
+"142": "https://picx.zhimg.com/80/v2-c6e6be5d29ebe5983ca388a3df8a371b_720w.png",
+"143": "https://picx.zhimg.com/80/v2-2cb06111c9144c9868fd87d866eb318b_720w.png",
+"144": "https://pic1.zhimg.com/80/v2-09a1abd1f5d19eacb6dfe4c1317f3482_720w.png",
+"145": "https://pic1.zhimg.com/80/v2-fc584dbc67b04951d17b3d24c198d21e_720w.png",
+"146": "https://pic1.zhimg.com/80/v2-25e71b0146b824f254a2e177e4aa48b0_720w.png",
+"147": "https://pic1.zhimg.com/80/v2-720464e052a742327b48847b67ff5a32_720w.png",
+"148": "https://pic1.zhimg.com/80/v2-80cee900e60825a403a4fc4c9526748c_720w.png",
+"149": "https://picx.zhimg.com/80/v2-0463016ed3f9c4f855d3a6f100412277_720w.png",
+"150": "https://pica.zhimg.com/80/v2-491d7e26b391cf2e5735b04f92812895_720w.png",
+"151": "https://picx.zhimg.com/80/v2-6f67756d958760d07f37bb6adeea22f2_720w.png",
+"152": "https://picx.zhimg.com/80/v2-ecf6fe1a383a13ae9f9ec15fd88e5d4b_720w.png",
+"153": "https://pica.zhimg.com/80/v2-5c959ceee79f53cb57baebfa4cc15414_720w.png",
+"154": "https://pic1.zhimg.com/80/v2-1d15a7ac025fd38900a2b5718d16e6cb_720w.png",
+"155": "https://pica.zhimg.com/80/v2-ce5a68c95368d041198fb2d2547e743d_720w.png",
+"156": "https://picx.zhimg.com/80/v2-2d5e8dc992679fa6bb80716f06e631e8_720w.png",
+"157": "https://picx.zhimg.com/80/v2-f4fd6fb0316b935b7a468bc2599f2b20_720w.png",
+"158": "https://pic1.zhimg.com/80/v2-8b9138b142983a0a3882d3626e347050_720w.png",
+"159": "https://picx.zhimg.com/80/v2-d39db9020d29b477eb015d5a64d5224b_720w.png",
+"160": "https://picx.zhimg.com/80/v2-4df903f186f07e48a71c595ac013d8bf_720w.png",
+"161": "https://picx.zhimg.com/80/v2-2f1a956d18925633791d73f695bfd806_720w.png",
+"162": "https://picx.zhimg.com/80/v2-20cd17c227f0fcad2fafa0ec3106eff5_720w.png",
+"163": "https://picx.zhimg.com/80/v2-4e94dde50293e9542e7639ae82fe3224_720w.png",
+"164": "https://picx.zhimg.com/80/v2-9a571876a6eed621366cf1fe9c4a02d4_720w.png",
+
+function showImage() {
+    var input = document.getElementById('customInput').value;
+    var img = document.getElementById('tp');
+    var passwordSection = document.getElementById('passwordSection'); 
+
+    if (codeToImage.hasOwnProperty(input)) {
+        img.src = codeToImage[input];
+        passwordSection.style.display = "block"; 
+    } else {
+        alert("暗号无效");
+    }
+}
